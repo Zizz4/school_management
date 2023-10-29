415139c7dcaa6d4191afd34b53ba1a7a77ab7620
unknown <syahrilazis2005@gmail.com>
2023-10-30 02:04:02 +0700

    [ADD] Validasi apabila save record yang sama

--- models/absensi.py
+++ models/absensi.py
@@ -1,6 +1,6 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 from datetime import date, datetime
-
+from odoo.exceptions import UserError
 
 class SchoolAbsensi(models.Model):
     _name = 'school.absensi'
@@ -16,7 +16,36 @@ class SchoolAbsensi(models.Model):
     kelas_id = fields.Many2one(string='Kelas', comodel_name='school.kelas', required=True)
     guru_id = fields.Many2one(string='Guru', comodel_name='res.users', domain=[('is_guru', '=', True)], required=True,
                               default=lambda self: self.env.user)
-    murid_ids = fields.Many2many('res.partner', domain=[('is_murid', '=', True)], string="Murid")
+
+    # ambil data murid
+    def _ambil_data_murid(self):
+        for rec in self:
+            if rec.kelas_id:
+                data = self.env['res.users'].search(rec.murid_ids, 'in', rec.kelas_id.murid_ids.ids)
+                rec.write({'murid_ids': [(4, data)]})
+            else:
+                rec.murid_ids = False
+
+    murid_ids = fields.Many2many('res.partner', domain=[('is_murid', '=', True)], string="Murid",
+                                 default=_ambil_data_murid)
+
+    # Check apabila ada pertemuan dan pelajaran yang sama
+    @api.model
+    def create(self, vals):
+        pertemuan = vals.get('name')
+        pelajaran = vals.get('pelajaran_id')
+        kelas = vals.get('kelas_id')
+
+        if pertemuan and pelajaran and kelas:
+            existing_record = self.search([
+                ('name', '=', pertemuan),
+                ('pelajaran_id', '=', pelajaran),
+                ('kelas_id', '=', kelas),
+            ])
+
+            if existing_record:
+                raise UserError(_("Cannot create a record with the same name and pelajaran"))
+        return super(SchoolAbsensi, self).create(vals)
 
     # Tampilkan Murid yang ada di kelas yang dipilih
     @api.onchange('kelas_id')
